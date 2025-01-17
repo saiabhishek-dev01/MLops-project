bdc5904f034a840d70d09b63a9643c87895528f6
saiabhishek19 <schakka@buffalo.edu>
2025-01-17 17:59:54 -0500

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from mlOPS import logger
 logger.info("Welcome to our custom logging")
+
+
+from mlOPS.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
