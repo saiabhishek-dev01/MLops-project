2df5631a8f6f5caa6924bc986b243b8205299f30
saiabhishek19 <schakka@buffalo.edu>
2025-01-17 18:31:47 -0500

    Data Validation Stage added

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ logger.info("Welcome to our custom logging")
 
 
 from mlOPS.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlOPS.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
